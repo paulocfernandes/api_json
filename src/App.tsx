c245bed327b8d63d7840348356404a0731564759
paulocfernandes <pauloregisc25@gmail.com>
2023-05-07 16:49:49 -0300

    api como metodo post

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, ChangeEvent } from 'react'
 import { Post } from './types/Post';
 
 function App() {
 
   const[posts, setPosts] = useState<Post[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const[addTitleText, setAddTitleText] = useState('');
+  const[addBodyText, setAddBodyText] = useState ('');
  
   useEffect(() => {
    loadPosts()
@@ -23,11 +26,71 @@ function App() {
    }  
   }
 
+  const handleTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setAddTitleText(e.target.value);
+  }
+
+    const handleBody = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      setAddBodyText(e.target.value)
+
+  }
+
+  const hadleAdd  = async ( ) => {
+
+    if(addTitleText && addBodyText) {
+      let response = await fetch ('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify ({
+          title:addTitleText,
+          body:addBodyText,
+          userId: 1 
+        }),
+        headers: {
+          'Content-Type ' : 'aplication/json'
+        }
+      });
+      let json = await response.json();
+      
+      if(json.id){
+        alert('Post adicionado com sucesso');
+      }else {
+        alert('Algo errado')
+      }
+
+    } else {
+      alert('Preencha os dados!')
+    }
+
+  }
+
   return (
     <div >  
       {loading && posts.length > 0 &&
       <div>Carregando</div>
       } 
+
+      <fieldset className='border-2 mb-3 mt-4 p-3'>
+        
+        <legend>Adicionar novo post</legend>
+        <input value={addTitleText}
+        onChange={handleTitle}
+         type="text" 
+         placeholder='Digite um título'
+          className='block border' />
+        
+        <textarea 
+        value={addBodyText}
+       onChange={handleBody}
+       className='block border'
+         ></textarea>
+
+        <button
+        onClick={hadleAdd}
+         className='block border'>
+          Adicionar
+          </button>
+      
+      </fieldset>
 
       
         {!loading && posts.length > 0 &&
