676de04319846bd9c92f772850808635af5862b7
paulocfernandes <pauloregisc25@gmail.com>
2023-05-21 11:55:02 -0300

    add biblioteca axios

--- src/api.ts
+++ src/api.ts
@@ -1,26 +1,41 @@
-const BASE = 'https://jsonplaceholder.typicode.com'
+import axios from "axios";
+
+const http = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+
 
 export const api =  {
-
-
  getAllPost :async() => {
-    let response =await fetch (`${BASE}/posts`);
+    let response= await http.get('/posts');
+    return response.data;
+
+
+    /*let response =await fetch (`${BASE}/posts`);
     let json = await response.json();
-    return json;
+    return json;*/
         
     },
 
     addNewPost: async(title:string, body: string,userId: number) => {
+        let response =await http.post('/posts', {
+            title,body,userId
+        });
+        return response.data;
 
-        let response = await fetch (`${BASE}/posts`, {
+    }
+    
+
+        /*let response = await fetch (`${BASE}/posts`, {
           method: 'POST',
           body: JSON.stringify ({title, body, userId }),
           headers: { 'Content-Type ': 'application/json'}
         });
         let json = await response.json();
-        return json;
+        return json;*/
         
-    }
+    
 
 
 
